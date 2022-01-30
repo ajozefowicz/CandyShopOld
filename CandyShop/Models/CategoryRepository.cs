1a1695346cd002ee69b768061d3d9bad323ccfc9
ajozefowicz <ginger4u@op.pl>
2022-01-30 18:04:13 +0100

    added CategoryRepository (last time was interfaces)

---
+++ CandyShop/Models/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> GetCategories => new List<Category>
+        {
+            new Category{CateogryId=1, CategoryName="Hard Candy", CategoryDescription="Super hard candy"},
+            new Category{CateogryId=2, CategoryName="Chocolate Candy", CategoryDescription="very chocolate candy"},
+            new Category{CateogryId=3, CategoryName="Fruit Candy", CategoryDescription="Sour fruit candy"},
+
+        }
+        ;
+    }
+}
